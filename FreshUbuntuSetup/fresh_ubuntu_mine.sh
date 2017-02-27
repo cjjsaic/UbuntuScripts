88ae91259d261e42121160d7543c965fb5e1bfbc
jinfagang <nicholasjela@gmail.com>
2017-02-27 19:19:04 +0800

    add

--- FreshUbuntuSetup/fresh_ubuntu_mine.sh
+++ FreshUbuntuSetup/fresh_ubuntu_mine.sh
@@ -1,25 +1,34 @@
 # !/bin/bash
 # this script for ubuntu16.04 for now
 
-# install pip3
-sudo apt update
-sudo apt install python3-pip
-
 # change pip source to tsinghua mirror
 cd ~
 mkdir .pip
 cd .pip
-pip_config_file="pip.cnf"
-if [ -f {pip_config_file} ]; then:
-	echo "pip.cnf aleady exist."
-	:> {pip_config_file}
-	echo "[global]" >> {pip_config_file}
-	echo "index-url = https://pypi.tuna.tsinghua.edu.cn/simple" >> {pip_config_file}
-	echo "[install]" >> {pip_config_file}
-	echo "trusted-host=mirrors.aliyun.com" >> {pip_config_file}
-else:
-	echo "[global]" >> {pip_config_file}
-	echo "index-url = https://pypi.tuna.tsinghua.edu.cn/simple" >> {pip_config_file}
-	echo "[install]" >> {pip_config_file}
-	echo "trusted-host=mirrors.aliyun.com" >> {pip_config_file}
-fi
+# install unity tweak tool
+sudo apt install unity-tweak-tool
+# set themes and tweak style like magic close window effect
+
+# install plank
+sudo apt install plank
+
+# install sublime
+sudo add-apt-repository ppa:webupd8team/sublime-text-3
+sudo apt-get update 
+sudo apt-get install sublime-text-installer
+
+## nemo file explorer
+sudo add-apt-repository ppa:webupd8team/nemo
+sudo apt-get update
+sudo apt-get install nemo nemo-fileroller
+gsettings set org.gnome.desktop.background show-desktop-icons false
+xdg-mime default nemo.desktop inode/directory application/x-gnome-saved-search
+
+# install numix theme
+sudo add-apt-repository ppa:numix/ppa  
+sudo apt-get update 
+sudo apt-get install numix-gtk-theme  
+sudo apt-get install numix-icon-theme-circle  
+sudo add-apt-repository ppa:noobslab/icons  
+sudo apt-get update  
+sudo apt-get install ultra-flat-icons 
