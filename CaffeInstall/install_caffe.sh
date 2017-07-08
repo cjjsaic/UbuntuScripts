789e0b14fd91ba1166abb2693d35f2049d09cf33
jinfagang <nicholasjela@gmail.com>
2017-07-08 15:18:26 +0800

    add docker

--- CaffeInstall/install_caffe.sh
+++ CaffeInstall/install_caffe.sh
@@ -4,7 +4,7 @@
 # and the cudnn
 cd ~
 git clone https://github.com/BVLC/caffe.git
-sudo apt-get install libprotobuf-dev libleveldb-dev libsnappy-dev libopencv-dev libboost-all-dev libhdf5-serial-dev libgflags-dev libgoogle-glog-dev liblmdb-dev protobuf-compiler libopenblas-dev
+sudo apt-get install libprotobuf-dev libleveldb-dev libsnappy-dev libopencv-dev libboost-all-dev libhdf5-serial-dev libgflags-dev libgoogle-glog-dev liblmdb-dev protobuf-compiler libopenblas-dev libatlas-base-dev
 cd caffe
 cp Makefile.config.example Makefile.config
 sed -i '{/USE_CUDNN/s/^# //g;/USE_CUDNN/s/0$/1/g}' Makefile.config
@@ -23,12 +23,16 @@ sed -i '{/USE_CUDNN/s/^# //g;/USE_CUDNN/s/0$/1/g}' Makefile.config
 sed -i '/INCLUDE_DIRS += $(BUILD_INCLUDE_DIR)/s/$/ \/usr\/include\/hdf5\/serial/g' Makefile
 sed -i '/LIBRARIES += glog gflags protobuf boost_system/s/hdf5/hdf5_serial/g' Makefile
 
-make all -j{`nproc`}
+make all -j8
 sudo apt-get install python-pip python-dev build-essential
 sudo pip3 install --upgrade pip
-sudo pip3 install -r --upgrade python/requirements.txt
+sudo pip3 install -r python/requirements.txt
 make pycaffe
 ./data/mnist/get_mnist.sh
 ./examples/mnist/create_mnist.sh
 ./examples/mnist/train_lenet.sh
 echo "Congratulations! Enjoy caffe!"
+
+
+# sometimes error says cannot find ...so.1.0 like this, you should
+# copy all library under /usr/local/cuda/lib64 to /usr/lib/
