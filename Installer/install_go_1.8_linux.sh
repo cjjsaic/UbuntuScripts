fe2daa408d92447c0cfa4d82a1549cc1f44d3e52
jinfagang <nicholasjela@gmail.com>
2017-07-09 08:13:24 +0800

    edit golang

--- Installer/install_go_1.8_linux.sh
+++ Installer/install_go_1.8_linux.sh
@@ -1,2 +1,5 @@
 wget https://storage.googleapis.com/golang/go1.8.3.linux-amd64.tar.gz | tar -C /usr/local -xzf go1.8.3.linux-amd64.tar.gz
 rm go1.8.3.linux-amd64.tar.gz
+echo Adding go path to global enviroment...
+echo export PATH=$PATH:/usr/local/go/bin >> ~/.profile
+echo Succed installed golang 1.8.3.
